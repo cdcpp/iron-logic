ca3fb57cfa9afe0daac92af0ca0d67e74850b071
cdcpp <oblilic93@gmail.com>
2026-04-16 22:01:21 +0900

    Initial commit : Iron-Logic

---
+++ src/main/java/com/ironlogic/ironlogic/IronlogicApplication.java
@@ -0,0 +1,13 @@
+package com.ironlogic.ironlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronlogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronlogicApplication.class, args);
+	}
+
+}
